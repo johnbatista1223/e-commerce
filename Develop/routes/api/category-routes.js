11422241a2a845d1fb2e8d42b256b39b346bfd3b
john batista <batistaj651@gmail.com>
2021-05-29 23:20:17 -0400

    fixing get route for id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const findAllData = await Category.findAll({ include: Product });
+    res.status(200).json(findAllData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+// be sure to include its associated Products
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  //   // find one category by its `id` value
+  //   // be sure to include its associated Products
+  try {
+    const findOneData = await Category.findOne(req.params.id, {
+      include: [{ Product }]
+    });
+
+    if (!findOneData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(findOneData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
